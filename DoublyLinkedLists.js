753183b6422bec83879121efbead27ad28bccdc8
Alexander Stratev <stoffalex@Alexanders-MacBook-Pro.local>
2021-07-03 18:16:55 -0500

    add more methods

--- DoublyLinkedLists.js
+++ DoublyLinkedLists.js
@@ -78,6 +78,83 @@ class DoublyLinkedLists {
         this.length++;
         return this;
     }
+
+//get specific element in the list based on given index
+    get(index) {
+        let middle = this.length / 2;
+        let currentHead = this.head;
+        let currentTail = this.tail;
+        let headCount = 0;
+        let tailCount = this.length - 1;
+        if (index < 0 || index >= this.length) {
+            return null;
+        } else if (index < middle) {
+            while (headCount < index) {
+                currentHead = currentHead.next;
+                headCount++;
+            }
+            return currentHead;
+        } else if (index > middle) {
+            while (tailCount > index) {
+                currentTail = currentTail.prev;
+                tailCount--;
+            }
+            return currentTail;
+        }
+    }
+
+//set new value at specific index in list
+    set(index, val) {
+        let foundNode = this.get(index);
+        if (!foundNode) {
+            return null;
+        } else {
+            foundNode.val = val;
+            return foundNode;
+        }
+    }
+
+//add element at specific index in list
+    insert (index, val) {
+        let newNode = new Node(val);
+        let prevNode = this.get(index - 1);
+        let nextNode = prevNode.next;
+        if (index < 0 || index > this.length) {
+            return false;
+        } else if (index === this.length) {
+            return !!this.push(val);
+        } else if (index === 0) {
+            return !!this.unshift(val);
+        } else {
+            prevNode.next = newNode;
+            newNode.prev = prevNode;
+            newNode.next = nextNode;
+            nextNode.prev = newNode;
+            this.length++;
+        }
+        return true;
+    }
+
+//remove element at specific index in list  
+    remove(index) {
+        let removedNode = this.get(index);
+        let prevNode = this.get(index - 1);
+        let nextNode = prevNode.next;
+        if (index < 0 || index >= this.length) {
+            return null;
+        } else if (index === 0) {
+            return this.shift();
+        } else if (index === this.length - 1) {
+            return this.pop();
+        } else {
+            removedNode.prev = null;
+            removedNode.next = null;
+            prevNode.next = nextNode;
+            nextNode.prev = prevNode;
+            this.length--;
+        }
+        return removedNode;
+    }
 }
 
 let list = new DoublyLinkedLists();
